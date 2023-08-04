d210abdfffcb2dfb4819506631ef294618f846ad
Art Karasev <as.karasyov@gmail.com>
2023-08-04 15:04:58 +1200

    site tree card

--- client/src/src/vue/App.vue
+++ client/src/src/vue/App.vue
@@ -10,6 +10,7 @@ import ServerCard from "./components/cards/ServerCard.vue";
 import GitCard from "./components/cards/GitCard.vue";
 import ComposerCard from "./components/cards/ComposerCard.vue";
 import GoogleAnalyticsCard from "./components/cards/GoogleAnalyticsCard.vue";
+import SiteTree from "./components/cards/SiteTree.vue";
 import Card from "./components/Card.vue";
 
 const props = defineProps({
@@ -27,26 +28,27 @@ const { call } = useRequestHandler({
 
     <div class="row row-cols-3">
 
-      <Card title="Brand card" :items="['logo', 'details', 'hotlinks', 'phones', 'emails', 'etc', 'social links icons']" />
-      <Card title="Handy tools" :items="['image compression', 'vide compression']" />
-      <Card title="Assets" :items="['last uploaded/amended', 'assets size']" />
-      <Card title="Pages" :items="['last edited']" />
-      <Card title="Data Objects" :items="['last edited']" />
-      <Card title="Blocks" :items="['last edited']" />
-      <Card title="Web Stats" :items="['environment', 'site access', 'assets', 'ss version', 'sitemap check/link', 'robots check/link', 'GTM/GTAG info', 'Facebook pixel', 'In Google Console?']" />
-      <Card title="Server stats" :items="['_SERVER', 'php', 'mysql', 'dns', '...']" />
-      <Card title="User" :items="['info', 'groups', 'last activity', 'mfa']" />
-      <Card title="Dev" :items="['dev/build', 'flush=1', 'dev/tasks', 'scheduled tasks','cron records']" />
+      <!-- <Card title="Brand card" :items="['logo', 'details', 'hotlinks', 'phones', 'emails', 'etc', 'social links icons']" /> -->
+      <!-- <Card title="Handy tools" :items="['image compression', 'vide compression']" /> -->
+      <!-- <Card title="Assets" :items="['last uploaded/amended', 'assets size']" /> -->
+      <!-- <Card title="Pages" :items="['last edited']" /> -->
+      <SiteTree />
+      <!-- <Card title="Data Objects" :items="['last edited']" /> -->
+      <!-- <Card title="Blocks" :items="['last edited']" /> -->
+      <!-- <Card title="Web Stats" :items="['environment', 'site access', 'assets', 'ss version', 'sitemap check/link', 'robots check/link', 'GTM/GTAG info', 'Facebook pixel', 'In Google Console?']" /> -->
+      <!-- <Card title="Server stats" :items="['_SERVER', 'php', 'mysql', 'dns', '...']" /> -->
+      <!-- <Card title="User" :items="['info', 'groups', 'last activity', 'mfa']" /> -->
+      <!-- <Card title="Dev" :items="['dev/build', 'flush=1', 'dev/tasks', 'scheduled tasks','cron records']" /> -->
 
-      <GoogleAnalyticsCard />
+      <!-- <GoogleAnalyticsCard /> -->
 
       <!--Packages cards-->
-      <Card title="Activecampaign/Mailchimp" :items="['latest activity']" />
-      <Card title="Tinified" :items="['active', 'last compressions with before/after size']" />
-      <Card title="SocialKit" :items="['refresh tokens', 'last fetch info', 'posts table info', 'manual fetch buttons']" />
-      <Card title="Form enquiries" :items="['latest']" />
-      <Card title="BugTracker" :items="['latest errors server/client']" />
-      <Card title="Performance" :items="['main score', 'latest tests']" />
+      <!-- <Card title="Activecampaign/Mailchimp" :items="['latest activity']" /> -->
+      <!-- <Card title="Tinified" :items="['active', 'last compressions with before/after size']" /> -->
+      <!-- <Card title="SocialKit" :items="['refresh tokens', 'last fetch info', 'posts table info', 'manual fetch buttons']" /> -->
+      <!-- <Card title="Form enquiries" :items="['latest']" /> -->
+      <!-- <Card title="BugTracker" :items="['latest errors server/client']" /> -->
+      <!-- <Card title="Performance" :items="['main score', 'latest tests']" /> -->
 
       <!-- <UserCard /> -->
       <!-- <ServerCard /> -->
---
+++ client/src/src/vue/components/cards/SiteTree.vue
@@ -0,0 +1,30 @@
+<script setup>
+
+import useRequestHandler from '../../composables/useRequestHandler'
+
+const { data, error, isFetching } = useRequestHandler({
+  url: '/api/dashcore/info/sitetree',
+});
+
+</script>
+<template>
+  <div class="col dashcard dashcard--sitetree" :class="isFetching ? 'dashcard--loading' : ''">
+    <div class="card mb-3">
+      <div class="card-header">
+        Pages
+        <a class="card-action" href="/admin/pages/add" target="_self"><i class="bi bi-plus-square-fill"></i></a>
+      </div>
+      <div class="card-body">
+        <ul class="dashcard__list" v-if="data && !isFetching">
+          <li v-for="item in data.list">
+            <a :href="item.link" :title="item.title" target="_self">
+              <i :class="item.icon"></i>
+              <span class="item__name" v-text="item.title"></span>
+            </a>
+            <span class="item__author" :title="item.updated_at"><span v-if="item.author">{{ item.author }}, </span>{{ item.updated_at_human }}</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
