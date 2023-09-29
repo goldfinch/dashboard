5c5ae23d568f2d04a874b45ae6575e6f4a65a25d
Art Karasev <as.karasyov@gmail.com>
2023-09-29 15:31:58 +1300

    add command

---
+++ src/Commands/DashboardCardCommand.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Goldfinch\Dashboard\Commands;
+
+use Goldfinch\Taz\Console\GeneratorCommand;
+use Goldfinch\Taz\Services\InputOutput;
+use Symfony\Component\Console\Command\Command;
+
+#[AsCommand(name: 'make:dashboard-card')]
+class DashboardCardCommand extends GeneratorCommand
+{
+    protected static $defaultName = 'make:dashboard-card';
+
+    protected $description = 'Create new dashboard card';
+
+    protected $path = 'app/src/Dashboard/Cards';
+
+    protected $type = 'dashboardcard';
+
+    protected $stub = './stubs/dashboardcard.stub';
+
+    protected $prefix = 'Card';
+
+    protected function execute($input, $output): int
+    {
+        parent::execute($input, $output);
+
+        return Command::SUCCESS;
+    }
+}
