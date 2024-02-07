9aa0d3cfe325a2571cf22529295d8ea589d7bd84
Art Karasev <2291826+goldfinch@users.noreply.github.com>
2024-02-07 14:26:14 +1300

    WIP

--- src/Commands/DashboardCardCommand.php
+++ src/Commands/DashboardCardCommand.php
@@ -3,7 +3,6 @@
 namespace Goldfinch\Dashboard\Commands;
 
 use Goldfinch\Taz\Console\GeneratorCommand;
-use Symfony\Component\Console\Command\Command;
 
 #[AsCommand(name: 'make:dashboard-card')]
 class DashboardCardCommand extends GeneratorCommand
@@ -19,11 +18,4 @@ class DashboardCardCommand extends GeneratorCommand
     protected $stub = './stubs/dashboardcard.stub';
 
     protected $prefix = 'Card';
-
-    protected function execute($input, $output): int
-    {
-        parent::execute($input, $output);
-
-        return Command::SUCCESS;
-    }
 }
