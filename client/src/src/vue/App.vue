b2986536c6d9fb0d2a370e634ed5f6f1a28f58c0
Art Karasev <2291826+goldfinch@users.noreply.github.com>
2024-02-21 11:22:46 +1300

    WIP

--- client/src/src/vue/App.vue
+++ client/src/src/vue/App.vue
@@ -3,6 +3,8 @@
 import useRequestHandler from './composables/useRequestHandler';
 // import { useSortable } from '@vueuse/integrations/useSortable'
 
+import LoadingComponent from './components/LoadingComponent.vue'
+
 const configUrl = '/admin/dashboard/Goldfinch-Dashboard-Configs-DashboardConfig/EditForm/field/Goldfinch-Dashboard-Configs-DashboardConfig/item/1/edit'
 
 const goSettings = () => {
@@ -28,9 +30,10 @@ const { data, error, isFetching } = useRequestHandler({
     </div> -->
     <div class="row">
         <div class="col-6"></div>
-        <div class="col-6 text-right"><a @click="goSettings" :href="configUrl">Settings</a></div>
+        <div class="col-6 text-right text-end"><a @click="goSettings" :href="configUrl">Settings</a></div>
     </div>
     <div class="row" v-if="data && !isFetching && data.cards != ''" v-html="data.cards"></div>
     <div v-else-if="data && !isFetching">No cards found</div>
+    <LoadingComponent v-else="isFetching" />
   </div>
 </template>
